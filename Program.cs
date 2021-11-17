babe4f26b49370ea0a4908e879797a7e2caed89d
Laken Harville <rlakenharville@gmail.com>
2021-11-16 22:48:11 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Try to guess my favorite number.");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (favNumber == userInput)
+            {
+                Console.WriteLine("You got it!");
+            }
+
+            else if (favNumber > userInput)
+            {
+                Console.WriteLine("Too low!");
+            }
+
+            else if (favNumber < userInput)
+            {
+                Console.WriteLine("Too high!");
+            }
+
+            else
+            {
+                Console.WriteLine("Nevermind.");
+            }
+
+            
+        }
+    }
+}
